9c2454a745b4b97c8776b40d9108e07a6a00fca0
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2022-09-25 21:34:58 -0600

    Needed_Inventory form dropdown is now alphabetical.

--- Inventories/forms.py
+++ Inventories/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import *
 from django.forms import ModelForm
+from django import forms
 
 
 # class Item_Form(ModelForm):
@@ -24,14 +25,44 @@ class Item_Form(ModelForm):
         labels = {'name': 'Item Name', 'quantity': 'Quantity', 'total_price': 'Total Price', }
 
 
+# class Needed_Inventory_Form(ModelForm):
+#     required_css_class = 'form-control'
+#     item = forms.ModelChoiceField(queryset=Item.objects.order_by('name'))
+#
+#     class Meta:
+#         model = Needed_Inventory
+#         fields = ['item']
+#         labels = {'item': 'Item Name'}
+#
+#         # widgets = {
+#         #     # 'item': forms.ComboField(),
+#         #     'item': forms.Select(attrs={'class': 'form-control'}),
+#         # }
+
 class Needed_Inventory_Form(ModelForm):
     class Meta:
         model = Needed_Inventory
+        ordering = ['item']
         fields = ('item',)
 
         widgets = {
-            # 'item': forms.ComboField(),
             'item': forms.Select(attrs={'class': 'form-control'}),
         }
 
         labels = {'item': 'Item Name'}
+
+    def __init__(self):
+        super(Needed_Inventory_Form, self).__init__()
+        self.fields['item'].queryset = self.fields['item'].queryset.order_by('name')
+
+# class Needed_Inventory_Form(ModelForm):
+#     class Meta:
+#         model = Needed_Inventory
+#         fields = ('item',)
+#
+#         widgets = {
+#             # 'item': forms.ComboField(),
+#             'item': forms.Select(attrs={'class': 'form-control'}),
+#         }
+#
+#         labels = {'item': 'Item Name'}
