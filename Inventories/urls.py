cc617e10cc8bde6840b7f5425751c5450fdb3e58
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2022-10-22 09:46:44 -0600

    Removed to_buy page and changed some styling on stock history page.

--- Inventories/urls.py
+++ Inventories/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('needed_inventory/', views.needed_inventory, name='needed_inventory'),
-    path('to_buy/', views.to_buy, name='to_buy'),
+    # path('to_buy/', views.to_buy, name='to_buy'),
     path('to_buy/<int:pk>/purchased/', views.purchased, name='purchased'),
     path('needed_inventory/<int:pk>/approve/', views.approve, name='approve'),
     path('needed_inventory/<int:pk>/delete/', views.delete, name='delete'),
